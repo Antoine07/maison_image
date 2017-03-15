3a744825bb850dbf2bd57deb703bec562b28d60a
Antoine07 <antoine.lucsko@wanadoo.fr>
2017-03-15 12:19:43 +0100

    wp config rules

---
+++ rules/wp-config-rules.php
@@ -0,0 +1,6 @@
+<?php
+
+define('WP_POST_REVISIONS', 2);
+
+// define('DISALLOW_FILE_EDIT',true); // désactive l'éditeur du thème
+// define('DISALLOW_FILE_MODS',true); // désactive l'installation des extensions
