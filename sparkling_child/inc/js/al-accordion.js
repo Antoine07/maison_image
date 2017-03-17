ec912effc9d9997befeb7b1e83959ddeb1ed5a24
Antoine07 <antoine.lucsko@wanadoo.fr>
2017-03-17 11:52:47 +0100

    test scrolling with mediaqueries

--- sparkling_child/inc/js/al-accordion.js
+++ sparkling_child/inc/js/al-accordion.js
@@ -1,20 +1,14 @@
 jQuery.noConflict();
-(function($){
+(function ($) {
 
-	$(function(){
-		 $(".accordion-toggle").on('click', function(e) {
+    $(function () {
+        $(".accordion-toggle").on('click', function (e) {
 
-		        e.preventDefault();
+            e.preventDefault();
 
-		        if(typeof $(".entry-content h3:first") !== 'undefined' && window.matchMedia('(max-width: 768px)').matches == false)
-		        {
-		            var pos = $(".entry-content h3:first").position();
+            $("html, body").animate({scrollTop: $(this).offset().top}, "slow");
 
-		            $("html, body").animate({ scrollTop: pos.top }, "slow");
-
-		        }
-
-		    })
-	});
+        })
+    });
 
 })(jQuery)
