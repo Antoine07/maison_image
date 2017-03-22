ca3134777308f24a45c4e71541ddffdefec49b03
Julien07 <your_juju-lien@wanadoo.fr>
2017-03-22 14:48:57 +0100

    migration

--- rules/migration.sql
+++ rules/migration.sql
@@ -1,13 +1,13 @@
 
 UPDATE db_options
-SET option_value = (replace(option_value, 'http://www.dev.maison-image.eu', 'http://www.maison-images.eu'))
+SET option_value = (replace(option_value, 'http://www.maison-images.eu', 'http://www.maison-image.eu'))
 WHERE option_name = 'home' OR option_name = 'siteurl';
 
 UPDATE db_posts
-SET guid = (replace(guid, 'http://www.dev.maison-image.eu','http://www.maison-images.eu'));
+SET guid = (replace(guid, 'http://www.maison-images.eu','http://www.maison-image.eu'));
 
 UPDATE db_posts
-SET post_content = (replace(post_content, 'http://www.dev.maison-image.eu', 'http://www.maison-images.eu'));
+SET post_content = (replace(post_content, 'http://www.maison-images.eu', 'http://www.maison-image.eu'));
 
 UPDATE db_postmeta
-SET meta_value = (replace(meta_value,'http://www.dev.maison-image.eu','http://www.maison-images.eu'));
+SET meta_value = (replace(meta_value,'http://www.maison-images.eu','http://www.maison-image.eu'));
